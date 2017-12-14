39137ee915f598ba3ec4d331eabc1c6280aa1dec
Demondre Livingston <livingstondemondre7@gmail.com>
2017-12-14 18:03:58 -0500

    Fully operational quakereport app that pulls live data

--- app/src/main/java/com/example/android/quakereport/CustomAdapter.java
+++ app/src/main/java/com/example/android/quakereport/CustomAdapter.java
@@ -1,6 +1,3 @@
-package com.example.android.quakereport;
-
-
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -16,60 +13,50 @@ package com.example.android.quakereport;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.quakereport;
 
-
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-import android.graphics.drawable.GradientDrawable;
-
-
-/*
-* {@link CustomAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
-* based on a data source, which is a list of {@link AndroidFlavor} objects.
-* */
-public class CustomAdapter extends ArrayAdapter<EarthquakeActivity> {
-    String primaryLocation;
-    String locationOffset;
-    private static final String LOG_TAG = CustomAdapter.class.getSimpleName();
+/**
+ * An {@link CustomAdapter} knows how to create a list item layout for each earthquake
+ * in the data source (a list of {@link Earthquake} objects).
+ * <p>
+ * These list item layouts will be provided to an adapter view like ListView
+ * to be displayed to the user.
+ */
+public class CustomAdapter extends ArrayAdapter<Earthquake> {
 
     /**
-     * This is our own custom constructor (it doesn't mirror a superclass constructor).
-     * The context is used to inflate the layout file, and the list is the data we want
-     * to populate into the lists.
-     *
-     * @param context     The current context. Used to inflate the layout file.
-     * @param earthquakes A List of AndroidFlavor objects to display in a list
+     * The part of the location string from the USGS service that we use to determine
+     * whether or not there is a location offset present ("5km N of Cairo, Egypt").
      */
-    public CustomAdapter(Activity context, ArrayList<EarthquakeActivity> earthquakes) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
+    private static final String LOCATION_SEPARATOR = " of ";
+
+    /**
+     * Constructs a new {@link CustomAdapter}.
+     *
+     * @param context     of the app
+     * @param earthquakes is the list of earthquakes, which is the data source of the adapter
+     */
+    public CustomAdapter(Context context, List<Earthquake> earthquakes) {
         super(context, 0, earthquakes);
     }
 
     /**
-     * Provides a view for an AdapterView (ListView, GridView, etc.)
-     *
-     * @param position    The position in the list of data that should be displayed in the
-     *                    list item view.
-     * @param convertView The recycled view to populate.
-     * @param parent      The parent ViewGroup that is used for inflation.
-     * @return The View for the position in the AdapterView.
+     * Returns a list item view that displays information about the earthquake at the given position
+     * in the list of earthquakes.
      */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -81,38 +68,62 @@ public class CustomAdapter extends ArrayAdapter<EarthquakeActivity> {
                     R.layout.list_item, parent, false);
         }
 
-
         // Find the earthquake at the given position in the list of earthquakes
-        EarthquakeActivity currentEarthquake = getItem(position);
-        String originalLocation = currentEarthquake.getLocation();
-
-        if (originalLocation.contains(LOCATION_SEPARATOR)) {
-            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
-            locationOffset = parts[0] + LOCATION_SEPARATOR;
-            primaryLocation = parts[1];
-        } else {
-            locationOffset = getContext().getString(R.string.near_the);
-            primaryLocation = originalLocation;
-        }
+        Earthquake currentEarthquake = getItem(position);
 
         // Find the TextView with view ID magnitude
         TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
-
-        //formats the magnitude in the one decimal place format
-        DecimalFormat decimalFormat = new DecimalFormat("0.0");
-
+        // Format the magnitude to show 1 decimal place
+        String formattedMagnitude = formatMagnitude(currentEarthquake.getMagnitude());
         // Display the magnitude of the current earthquake in that TextView
-        magnitudeView.setText(decimalFormat.format(currentEarthquake.getManitude()));
+        magnitudeView.setText(formattedMagnitude);
 
-        // Find the TextView with view ID primary location
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+        // Get the original location string from the Earthquake object,
+        // which can be in the format of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
+        String originalLocation = currentEarthquake.getLocation();
+
+        // If the original location string (i.e. "5km N of Cairo, Egypt") contains
+        // a primary location (Cairo, Egypt) and a location offset (5km N of that city)
+        // then store the primary location separately from the location offset in 2 Strings,
+        // so they can be displayed in 2 TextViews.
+        String primaryLocation;
+        String locationOffset;
+
+        // Check whether the originalLocation string contains the " of " text
+        if (originalLocation.contains(LOCATION_SEPARATOR)) {
+            // Split the string into different parts (as an array of Strings)
+            // based on the " of " text. We expect an array of 2 Strings, where
+            // the first String will be "5km N" and the second String will be "Cairo, Egypt".
+            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
+            // Location offset should be "5km N " + " of " --> "5km N of"
+            locationOffset = parts[0] + LOCATION_SEPARATOR;
+            // Primary location should be "Cairo, Egypt"
+            primaryLocation = parts[1];
+        } else {
+            // Otherwise, there is no " of " text in the originalLocation string.
+            // Hence, set the default location offset to say "Near the".
+            locationOffset = getContext().getString(R.string.near_the);
+            // The primary location will be the full location string "Pacific-Antarctic Ridge".
+            primaryLocation = originalLocation;
+        }
+
+        // Find the TextView with view ID location
         TextView primaryLocationView = (TextView) listItemView.findViewById(R.id.primary_location);
         // Display the location of the current earthquake in that TextView
         primaryLocationView.setText(primaryLocation);
 
-        //Find the TextView with View ID locationOffset
-        TextView locationOffsetSetView = (TextView) listItemView.findViewById(R.id.location_offset);
-        //Display the offset of the location the earthquake is closet to
-        locationOffsetSetView.setText(locationOffset);
+        // Find the TextView with view ID location offset
+        TextView locationOffsetView = (TextView) listItemView.findViewById(R.id.location_offset);
+        // Display the location offset of the current earthquake in that TextView
+        locationOffsetView.setText(locationOffset);
 
         // Create a new Date object from the time in milliseconds of the earthquake
         Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
@@ -130,20 +141,16 @@ public class CustomAdapter extends ArrayAdapter<EarthquakeActivity> {
         String formattedTime = formatTime(dateObject);
         // Display the time of the current earthquake in that TextView
         timeView.setText(formattedTime);
-        // Set the proper background color on the magnitude circle.
-        // Fetch the background from the TextView, which is a GradientDrawable.
-        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
 
-        // Get the appropriate background color based on the current earthquake magnitude
-        int magnitudeColor = getMagnitudeColor(currentEarthquake.getManitude());
-
-        // Set the color on the magnitude circle
-        magnitudeCircle.setColor(magnitudeColor);
         // Return the list item view that is now showing the appropriate data
-
         return listItemView;
     }
 
+    /**
+     * Return the color for the magnitude circle based on the intensity of the earthquake.
+     *
+     * @param magnitude of the earthquake
+     */
     private int getMagnitudeColor(double magnitude) {
         int magnitudeColorResourceId;
         int magnitudeFloor = (int) Math.floor(magnitude);
@@ -180,10 +187,18 @@ public class CustomAdapter extends ArrayAdapter<EarthquakeActivity> {
                 magnitudeColorResourceId = R.color.magnitude10plus;
                 break;
         }
+
         return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 
-    private static final String LOCATION_SEPARATOR = " of ";
+    /**
+     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
+     * from a decimal magnitude value.
+     */
+    private String formatMagnitude(double magnitude) {
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
+    }
 
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
